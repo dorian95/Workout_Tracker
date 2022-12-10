dfcad3292687874d0eed9637c50da38073a378ec
Nursultan Irgaliyev <nursultan.irgaliyev@gmail.com>
2022-12-10 14:59:29 -0800

    added  getWorkouts to controller code

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,10 @@
 // to get access to app we need to use express router
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+    createWorkout,
+    
+} = require('../controllers/workoutController')
+
 const mongoose = require('mongoose')
 // create an instance of Router
 const router = express.Router()
@@ -18,17 +22,7 @@ router.get('/:id', (req, res) => {
 
 
 // POST a new workout
-router.post('/', async (req, res) => {
-    const {title, load, reps} = req.body
-
-    try {
-        // async call
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    } 
-})
+router.post('/', createWorkout)
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
